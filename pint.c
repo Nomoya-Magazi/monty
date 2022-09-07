e80bce700a71f8042710b5c69d64e59664856f41
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-07 17:34:50 +0200

    Update pint.c

--- pint.c
+++ pint.c
@@ -8,3 +8,13 @@
  */
 void f_pint(stack_t **head, unsigned int number)
 {
+if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
