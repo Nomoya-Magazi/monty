b80e85b241c413dc19c590489b13068192c1c431
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-07 18:11:07 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
- * f_push - function that pushes an element to the stack
- * @head: top
+ * f_push - add node to the stack
+ * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 void f_push(stack_t **head, unsigned int counter)
 {
-int n, j = 0, flag = 0;
+	int n, j = 0, flag = 0;
 
 	if (bus.arg)
 	{
