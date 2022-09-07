c367d35fa4512db9c067acf351f30f73d822186f
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-07 18:10:02 +0200

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
- * f_pop - the function that removes the top element of the stack
- * @head: the stack head
- * @counter:line_number
+ * f_pop - prints the top
+ * @head: stack head
+ * @counter: line_number
  * Return: no return
- */
+*/
 void f_pop(stack_t **head, unsigned int counter)
 {
-stack_t *h;
+	stack_t *h;
 
 	if (*head == NULL)
 	{
