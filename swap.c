823657403fceaf76af1b7a9b3e1a5066c480d88d
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-07 18:12:16 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
- * f_swap -opcode that swaps the top two elements of the stack
- * @head: the top of the stack
+ * f_swap - adds the top two elements of the stack.
+ * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 void f_swap(stack_t **head, unsigned int counter)
 {
-stack_t *h;
+	stack_t *h;
 	int len = 0, aux;
 
 	h = *head;
