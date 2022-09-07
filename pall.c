43cb27e072054e02555c356baf731b8b2184fa23
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-07 18:08:24 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
- * f_pall - a function that prints all the values on the stack
- * @head: top
- * @counter: no conter
- * Return: dont return
- */
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
 void f_pall(stack_t **head, unsigned int counter)
 {
-stack_t *h;
+	stack_t *h;
 	(void)counter;
 
 	h = *head;
