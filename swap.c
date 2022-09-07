c97c82a61f9f1894363b8413039d285122e9a16e
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-09-07 08:43:19 -0700

    running betty

--- swap.c
+++ swap.c
@@ -2,7 +2,7 @@
 /**
  * f_swap -opcode that swaps the top two elements of the stack
  * @head: the top of the stack
- * @counter; line_number
+ * @counter: line_number
  * Return: no return
  */
 void f_swap(stack_t **head, unsigned int counter)
